5cd9e82388bcf2b66f41830988236b3ab04b1744
SpaceDog777 <dannysplace@gmail.com>
2020-09-06 13:53:54 +1200

    Message

--- Test/src/Test/HelloWorld.java
+++ Test/src/Test/HelloWorld.java
@@ -2,8 +2,7 @@ package Test;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {		
 		System.out.println("Hello World");
 	}
 
